aaf89bb49b2d021855a8fc2c177aff5490175206
Diego <colmop1548@gmail.com>
2026-09-16 22:34:11 -0500

    feat: initial landing page

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#4A5539',
+                'primary-hover': '#3d4630',
+                section: '#F7F8F9',
+                main: '#1A1A1A',
+                graytext: '#6B7280',
+                tagbg: '#FDF6E3', // Brown/goldish tint for tag background
+                tagtext: '#926C15'
+            },
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+                display: ['Poppins', 'sans-serif'],
+                mono: ['JetBrains Mono', 'monospace'],
+            }
+        }
+    }
+};
